c982fcf5e4c0eba70c25a26d9a6982087fb30c43
Peterson Dourado <petyds@gmail.com>
2025-04-03 18:58:11 -0300

    correção conexão com banco de dados

--- admin/conexao.php
+++ admin/conexao.php
@@ -1,46 +1,12 @@
-<!DOCTYPE html>
-<!-- Source Codes By CodingNepal - www.codingnepalweb.com -->
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Administração |  Login</title>
-  <link rel="stylesheet" href="../css/bootstrap.min.css">
-  <!-- <link rel="stylesheet" href="../css/login.css" /> -->
-</head>
-<body>
-
 <?php
-$host = "localhost"; // Servidor do banco de dados
-$dbname = "hospital_db"; // Nome do banco de dados
-$username = "root"; // Nome de usuário do MySQL
-$password = ""; // Senha do MySQL
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hospital_db";
 
-$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-
-//echo "conexao bem sucedida!";
-
-//$sql = "SELECT * FROM usuarios";
-$sql = "SELECT * FROM usuarios";
-$result = $pdo->query($sql);
-
-foreach ($result as $row) {
-    echo "ID: " . $row['id'] . " - Nome: " . $row['nome'] . " - tipo: " . $row['tipo'] . " - Email: " . $row['email'] . "<br>";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
 }
 
-// try {
-//     // Criar conexão usando PDO
-//     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    
-//     // Configurar para lançar exceções em caso de erro
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-//     echo "<h2 class='text-center text-success'>Conexão bem-sucedida!</h2>";
-// } catch (PDOException $e) {
-//     echo "Erro na conexão: " . $e->getMessage();
-// }
-
 ?>
-
-</body>
-</html>
